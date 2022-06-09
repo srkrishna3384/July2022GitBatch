ea029120b38cc906a6895582313f66e150fb6dd8
srkrishna3384 <srkrishna3384@gmail.com>
2022-06-09 09:31:39 +0530

    Added GitBatchJuly2022 project

---
+++ src/main/java/com/rameshsoft/test/GitBatchJuly2022/App.java
@@ -0,0 +1,13 @@
+package com.rameshsoft.test.GitBatchJuly2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/rameshsoft/test/GitBatchJuly2022/TestDemo.java
@@ -0,0 +1,19 @@
+package com.rameshsoft.test.GitBatchJuly2022;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	@Test
+	public void testOne() {
+		System.out.println("TestONe...");
+	}
+	@Test
+	public void testTwo() {
+		System.out.println("TestTwo...");
+	}
+	@Test
+	public void testThree() {
+		System.out.println("TestThree...");
+	}
+
+}
